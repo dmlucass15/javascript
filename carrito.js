1c30db42481786de7bcc899c652100b53e8a61ba
dmlucass15 <dmlucass15@gmail.com>
2023-07-28 13:25:47 -0500

    anteultimo js

--- carrito.js
+++ carrito.js
@@ -1,29 +1,13 @@
-const arrayProductos= [
-    {imagen: 'img/muñeca', nombre:'Muñequera', precio: '$5.000',   codigo: 1},
-    {imagen: 'img/gym.avif', nombre:'Plan Gym', precio: '$8.000',   codigo: 2   },
-    {imagen: 'img/dietario', nombre:'Plan Dietario', precio: '$5.000',   codigo: 3 },
-    {imagen: 'img/gymdietario.jpg', nombre:'Plan Gym + Dietario', precio: '$10.000', codigo: 4},
-    {imagen: 'img/proteina', nombre:'Proteina Whey', precio: '$30.000',  codigo: 5 },
-    {imagen: 'img/musculosa', nombre:'Musculosa Shark', precio: '$15.000', codigo: 6  },    
-]
 
-function obtenerCarro(){
-    JSON.parse(localStorage.getItem('carritodeCompras'))
-    if(localStorage.getItem('carritodeCompras')!== null){
-        return JSON.parse(localStorage.getItem('carritodeCompras'))
-    } else {
-        return []
-    }
-}
-
-const carritodeCompras = obtenerCarro()
+const Carrito=  JSON.parse(localStorage.getItem('carritodeCompras'))
+console.log(Carrito)
 
 const tBody = document.querySelector('#tbody')
 
 
 function casiterminadoCarrito (things){
     return `<tr>
-    <td>${things.imagen}</td>
+    <td> <img src="${things.imagen}"></td>
     <td>${things.nombre}</td>
     <td>${things.precio}</td>
     <td>${things.codigo}</td>
@@ -33,8 +17,8 @@ function casiterminadoCarrito (things){
 
 function listoCarrito (){
     tBody.innerHTML = ''
-    if(carritodeCompras.length > 0) {
-        carritodeCompras.forEach ((things)=> {
+    if(Carrito.length > 0) {
+        Carrito.forEach ((things)=> {
                 tBody.innerHTML +=  casiterminadoCarrito (things)
 
         }
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const arrayProductos= [
+    {imagen: 'img/muñeca', nombre:'Muñequera', precio: '$5.000',   codigo: 1},
+    {imagen: 'img/gym.avif', nombre:'Plan Gym', precio: '$8.000',   codigo: 2   },
+    {imagen: 'img/dietario', nombre:'Plan Dietario', precio: '$5.000',   codigo: 3 },
+    {imagen: 'img/gymdietario.jpg', nombre:'Plan Gym + Dietario', precio: '$10.000', codigo: 4},
+    {imagen: 'img/proteina', nombre:'Proteina Whey', precio: '$30.000',  codigo: 5 },
+    {imagen: 'img/musculosa', nombre:'Musculosa Shark', precio: '$15.000', codigo: 6  },    
+]
+
 
 const contenedor = document.querySelector('#contenedor')
 const buscar= document.querySelector('#inputSearch')
 
 
-
+const carritodeCompras = JSON.parse(localStorage.getItem('carritodeCompras'))
 
 
 
@@ -27,7 +36,7 @@ function tocarBotones(){
         let producto= arrayProductos.find((prod)=> prod.codigo ==boton.id)
         carritodeCompras.push(producto)
         localStorage.setItem('carritodeCompras', JSON.stringify(carritodeCompras))
-        console.log(producto)
+        console.log(carritodeCompras)
         })
     })
 }
@@ -39,7 +48,6 @@ function subimosCajas (array) {
             contenedor.innerHTML += armamosCajas(caja)
             tocarBotones()
     })
-   
 }
 
 subimosCajas(arrayProductos)
